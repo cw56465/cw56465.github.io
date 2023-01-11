a6465fa77fd491341c1e2c452b9efd1eba9fe81a
cw56465 <3057374377@qq.com>
2023-01-11 19:31:06 +0800

    Site updated: 2023-01-11 19:31:06

---
+++ js/weather.js
@@ -0,0 +1,1 @@
+WIDGET={CONFIG:{modules:"012",background:"5",tmpColor:"FFFFFF",tmpSize:"16",cityColor:"FFFFFF",citySize:"16",aqiColor:"FFFFFF",aqiSize:"16",weatherIconSize:"24",alertIconSize:"18",padding:"10px 10px 10px 10px",shadow:"1",language:"auto",borderRadius:"20",fixed:"false",vertical:"top",horizontal:"left",key:"375a5e8a871447df8f63c24805ffb9ab"}};
